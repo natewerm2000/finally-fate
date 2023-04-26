a2cb9dec12aef19a27416022a2052616442a470a
natewerm2000 <89500945+natewerm2000@users.noreply.github.com>
2023-04-25 20:49:52 -0400

    sick bg

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet"></link>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,34 @@ export default function home(){
             
             
             <Navbar li={[
-        ["Dashboard", "img/dashboard.svg"],
+        ["Dashboard", ""],
         ["Brendan’s Victims", ""],
         ["Brendan's Dick pics", ""],
         ["Brendan's N pass ", ""],
         ["Brendan's penis stats", ""]
       ]}
     />
-    <Time />
     
+    <Time />
+    <div className="intro">
+        <p id='head1' class='header'>Finally Fate</p>
+<p id='head2' class='header'>Finally here</p>
+<p id='head3' class='header'>For you</p>
+<p id='head4' class='header'>all of your needs</p>
+<p id='head5' class='header'>beautifully crafted</p>
+    </div>
+   
+<button>Continue</button>
+    <div class='light x1'></div>
+  <div class='light x2'></div>
+  <div class='light x3'></div>
+  <div class='light x4'></div>
+  <div class='light x5'></div>
+  <div class='light x6'></div>
+  <div class='light x7'></div>
+  <div class='light x8'></div>
+  <div class='light x9'></div>
+  
         </div>
     )
 }
